3451b18f8f4c510a08151b5978dae28888f6c7f4
lintool <jimmylin@umd.edu>
2013-12-03 04:52:09 -0500

    implements LoadMetadata

--- src/main/java/org/warcbase/pig/ArcLoader.java
+++ src/main/java/org/warcbase/pig/ArcLoader.java
@@ -9,18 +9,25 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.pig.Expression;
 import org.apache.pig.FileInputLoadFunc;
+import org.apache.pig.LoadMetadata;
 import org.apache.pig.PigException;
+import org.apache.pig.ResourceSchema;
+import org.apache.pig.ResourceStatistics;
 import org.apache.pig.backend.executionengine.ExecException;
 import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigSplit;
+import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+import org.apache.pig.impl.logicalLayer.schema.Schema.FieldSchema;
 import org.jwat.arc.ArcRecordBase;
 import org.warcbase.mapreduce.ArcInputFormat;
 
 import com.google.common.collect.Lists;
 
-public class ArcLoader extends FileInputLoadFunc {
+public class ArcLoader extends FileInputLoadFunc implements LoadMetadata {
   private static final TupleFactory TUPLE_FACTORY = TupleFactory.getInstance();
 
   private RecordReader<LongWritable, ArcRecordBase> in;
@@ -36,7 +43,7 @@ public class ArcLoader extends FileInputLoadFunc {
   @Override
   public Tuple getNext() throws IOException {
     try {
-      if ( !in.nextKeyValue() ) {
+      if (!in.nextKeyValue()) {
         return null;
       }
 
@@ -50,7 +57,8 @@ public class ArcLoader extends FileInputLoadFunc {
 
       // Only grab content text content, ignore binary data.
       if (type.toLowerCase().contains("text")) {
-        protoTuple.add(new String(IOUtils.toByteArray(record.getPayloadContent()), Charset.forName("UTF-8")));
+        protoTuple.add(new String(IOUtils.toByteArray(record.getPayloadContent()), 
+            Charset.forName("UTF-8")));
       } else {
         // Otherwise add empty string to preserve consistent schema.
         protoTuple.add("");
@@ -73,5 +81,31 @@ public class ArcLoader extends FileInputLoadFunc {
   @Override
   public void setLocation(String location, Job job) throws IOException {
     FileInputFormat.setInputPaths(job, location);
+  }
+
+  @Override
+  public String[] getPartitionKeys(String location, Job job) throws IOException {
+    return null;
+  }
+
+  @Override
+  public ResourceSchema getSchema(String location, Job job) throws IOException {
+    List<FieldSchema> fieldSchemaList = Lists.newArrayList();
+
+    fieldSchemaList.add(new FieldSchema("url", DataType.CHARARRAY));
+    fieldSchemaList.add(new FieldSchema("date", DataType.CHARARRAY));
+    fieldSchemaList.add(new FieldSchema("mime", DataType.CHARARRAY));
+    fieldSchemaList.add(new FieldSchema("content", DataType.CHARARRAY));
+
+    return new ResourceSchema(new Schema(fieldSchemaList));
+  }
+
+  @Override
+  public ResourceStatistics getStatistics(String location, Job job) throws IOException {
+    return null;
+  }
+
+  @Override
+  public void setPartitionFilter(Expression partitionFilter) throws IOException {
   }
 }
